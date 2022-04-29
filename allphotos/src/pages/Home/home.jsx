a7fde0a7c9d52f2a86d5ab812ebc960a0559806f
Fernando <ribeiro.fer50@gmail.com>
2022-04-29 11:52:15 -0300

    pagina home

--- allphotos/src/pages/Home/home.jsx
+++ allphotos/src/pages/Home/home.jsx
@@ -1,6 +1,14 @@
+import './home.css'
 
 export const Home = () => {
   return (
-      <h1>Homess</h1>
+      <div id="homepage">
+        <img src="/src/assets/BackgroundImage.png" id="backgroundImg" ></img>
+        <div id="navbar-home">
+          <button id='photosUpload'>+</button>
+          <p id='username-navbar'>Username</p>
+          <button id='btn-profile'><img src="/src/assets/imgProfile.jpeg" id='imgProfile'/></button>
+        </div>     
+      </div>
   )
 }
