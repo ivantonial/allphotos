74f979410df27e53d09f35255882695bad68c80c
FelipeTRpo <felipetrpoke@hotmail.com>
2022-04-30 16:11:31 -0300

    navbar home with logo and home pics displayed

--- allphotos/src/pages/Home/home.jsx
+++ allphotos/src/pages/Home/home.jsx
@@ -5,10 +5,32 @@ export const Home = () => {
       <div id="homepage">
         <img src="/src/assets/BackgroundImage.png" id="backgroundImg" ></img>
         <div id="navbar-home">
-          <button id='photosUpload'>+</button>
-          <p id='username-navbar'>Username</p>
-          <button id='btn-profile'><img src="/src/assets/imgProfile.jpeg" id='imgProfile'/></button>
-        </div>     
+          <div id="right-homebar-area">
+            <img src="/src/assets/Logo.svg" alt="" id='logo-svg-homebar'/>
+            <p>AllPhotos</p>
+          </div>
+          <div id="left-homebar-area">
+            <button id='photosUpload'>+</button>
+            <p id='username-navbar'>Username</p>
+            <button id='btn-profile'><img src="/src/assets/imgProfile.jpeg" id='imgProfile'/></button>
+          </div>
+        </div>
+        <div id='photos-display'>
+          <div className='photo-box'>
+            <div className="photo-box-header">
+              <img src="/src/assets/imgProfile.jpeg" alt="" /> 
+              <p>Username</p>
+              </div>
+            <div className="photo-box-body"><img src="/src/assets/galolindo.png" alt="" /></div>
+            <div className="photo-box-footer"></div>
+          </div>
+          <div className='photo-box'></div>
+          <div className='photo-box'></div>
+          <div className='photo-box'></div>
+          <div className='photo-box'></div>
+          <div className='photo-box'></div>
+          
+        </div>
       </div>
   )
 }
